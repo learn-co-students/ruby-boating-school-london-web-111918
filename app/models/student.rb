835b73aa16a29cd421e93fce44aec3d161b43dea
github username <github email address>
2018-11-23 13:19:50 +0000

    Completed Boating School Lab

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,27 @@
 class Student
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(name, status, instructor)
+    BoatingTest.new(self, name, status, instructor)
+  end
+
+  def self.find_student(name)
+    self.all.find {|student| student.name == name}
+  end
+
+  def grade_percentage
+  end
+
 end
