7e468b58a41e8cd05fde3c5eab9a29a668d5b0c9
ja9-look <janineluk95@gmail.com>
2018-11-26 17:26:05 +0000

    completed boating school challenge

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,27 @@
 class Instructor
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  def self.pass_student(first_name, boating_test)
+    test_to_update = BoatingTest.all.find {|bt| bt.student.first_name == first_name && bt.status != "passed"}
+    test_to_update.status = "passed"
+  end
+
+  def self.fail_student(first_name, boating_test)
+    test_to_update = BoatingTest.all.find {|bt| bt.student.first_name == first_name && bt.status != "failed"}
+    test_to_update.status = "failed"
+  end
+
 end
