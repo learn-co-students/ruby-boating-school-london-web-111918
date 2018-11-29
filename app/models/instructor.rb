2a9f04efee03947c0ba98e3edc5f09df893d073b
jodyneckles <jody@technative.tv>
2018-11-29 18:20:11 +0000

    solved

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,39 @@
 class Instructor
 
+  attr_accessor :name
+
+  @@all = []
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(name, test)
+    BoatingTest.all.each do |boat_test|
+       # binding.pry
+      if boat_test.student.name == name && boat_test.test == test
+        boat_test.status = "passed"
+      else
+        boat_test = BoatingTest.new(name, test, "passed", self)
+      end
+      return boat_test
+    end
+  end
+
+  def fast_student(name, test)
+    BoatingTest.all.each do |boat_test|
+       # binding.pry
+      if boat_test.student.name == name && boat_test.test == test
+        boat_test.status = "failed"
+      else
+        boat_test = BoatingTest.new(name, test, "failed", self)
+      end
+      return boat_test
+    end
+  end
+
 end
