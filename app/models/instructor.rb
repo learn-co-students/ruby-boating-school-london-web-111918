6f3c58c102e40453494f0decf240f56a1081e772
github username <github email address>
2018-12-03 21:52:31 +0000

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,39 @@
 class Instructor
+  attr_accessor :name
+  attr_writer :test_status
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(student, test_name)
+    result =  BoatingTest.all.find do |boating_test|
+      boating_test.student == student && boating_test.test_name == test_name
+    end
+    if !result
+      BoatingTest.new(student, test_name, test_status = "passed", self)
+    else
+      result.test_status = "passed"
+    end
+  end
+
+
+  def fail_student(student, test_name)
+      result = BoatingTest.all.find do |boating_test|
+      boating_test.student == student && boating_test.test_name == test_name
+      end
+      if !result
+        BoatingTest.new(student, test_name, test_status = "failed", self)
+      else
+        result.test_status = "failed"
+      end
+  end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,35 @@
 class Student
+  attr_accessor :first_name
 
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(test_name, test_status, instructor)
+    BoatingTest.new(self, test_name, test_status, instructor)
+  end
+
+  # def self.find_student(first_name)
+  #   chosen_one = []
+  #   chosen_one << self.all.select { |student| student.first_name = first_name }
+  #   chosen_one
+  # end
+
+  def self.find_student(first_name)
+    @@all.select { |student| student.first_name = first_name }
+  end
+
+  def grade_percentage
+    all_their_tests = BoatingTest.all.select {|boating_test| boating_test.student == self }
+    passed_tests = all_their_tests.select {|boating_test| boating_test.test_status == "passed"}
+    passed_tests.length.to_f / all_their_tests.length.to_f
+  end
+  
 end
